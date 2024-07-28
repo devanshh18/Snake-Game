323d7d4b1b157a8a52fcd41a286fa97e6ebdcdaf
devanshh18 <devanshpatil2202@gmail.com>
2024-07-28 16:56:13 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vt323: ["VT323", "monospace"],
+      },
+      gridTemplateColumns: {
+        "20-cols": "repeat(20, 20px)",
+      },
+      gridTemplateRows: {
+        "20-rows": "repeat(20, 20px)",
+      },
+      boxShadow: {
+        "inset-green1": "inset 0 0 0 10px #595f43",
+        "inset-green2": "inset 0 0 0 10px #abb78a",
+        "inset-green3": "inset 0 0 0 5px #8b966c",
+      },
+      inset: {
+        "3/5": "60%",
+      },
+      colors: {
+        'custom-gray': '#333'
+      }
+    },
+  },
+  plugins: [],
+};
